7f8bd6541ed5553d8b106b295c32dd14d18ce61c
ShreeMaiya <shreemaiya247@gmail.com>
2024-12-19 08:00:05 +0530

    Update

--- GetStego/settings.py
+++ GetStego/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Ensure this is a valid path
+
+# Add the templates/ directory to STATICFILES_DIRS
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
